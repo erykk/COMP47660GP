c027dc6281d76ac0e1743a4aa9cc39f61f27ee4a
Barry <barry.murphy@ucdconnect.ie>
2021-02-15 16:14:54 +0000

    Created FlightController.java & added some test data

---
+++ src/main/java/ie/ucd/COMP47660GP/controller/FlightController.java
@@ -0,0 +1,42 @@
+package ie.ucd.COMP47660GP.controller;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+@RestController
+public class FlightController{
+
+    static int referenceNumber;    // Temp for testing purposes
+    private static Map<Integer, String> flights = new TreeMap<>();    // Temp for testing purposes
+    private static String [] emails =  new String[5];               // Temp for testing purposes
+
+    static {   // Temp for testing purposes
+        flights.put(1,"Paris");
+        flights.put(2,"Madrid");
+        flights.put(3,"New York");
+    }
+
+
+}
