c5a05824fcb453d1aaefe532aa35324a0ce54785
erykkk <eryk.pilat@ucdconnect.ie>
2021-03-03 17:53:18 +0000

    fixed typo in config

--- src/main/java/ie/ucd/COMP47660GP/config/SecurityConfig.java
+++ src/main/java/ie/ucd/COMP47660GP/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         http.authorizeRequests().antMatchers("/register","/","/login", "/secureRegister", "/secureLogin",
-                "/resources/**", "/images/**", "/reservation", "/flight", "/create-reservation", "/create-reservation/*", "/deleteAll", "/success", "/fail").permitAll()
+                "/resources/**", "/images/**", "/reservation", "/flight", "/create-reservation", "/create-reservation/*", "/deleteAccount", "/success", "/fail").permitAll()
                 //.anyRequest().authenticated()
                 .and()
                 .formLogin()
