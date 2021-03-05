8f0e48a228a33ece84c3e7930255f27a34d484c6
erykkk <eryk.pilat@ucdconnect.ie>
2021-03-05 22:35:53 +0000

    +: simple error controller

---
+++ src/main/java/ie/ucd/COMP47660GP/controller/CustomErrorController.java
@@ -0,0 +1,24 @@
+package ie.ucd.COMP47660GP.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public String handleError(Model model){
+        return "Oops, you did something wrong. Click back!";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
