61797b8bceb6822af66ae24637b7e6b0bc5b9987
murphybt <barry.murphy@ucdconnect.ie>
2021-05-16 15:08:55 +0100

    add filter directory with several new files, added ACUserDetails

---
+++ src/main/java/ie/ucd/COMP47660GP/service/impl/ACUserDetails.java
@@ -0,0 +1,66 @@
+package ie.ucd.COMP47660GP.service.impl;
+
+import ie.ucd.COMP47660GP.entities.Role;
+import ie.ucd.COMP47660GP.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class ACUserDetails implements UserDetails {
+
+    private User user;
+
+    public ACUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        if(roles != null) {
+            for (Role role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+            }
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
