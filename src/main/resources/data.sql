ae7e8c96e3398fbcd676e453f1448e7545a0bdfe
erykk <eryk.pilat@ucdconnect.ie>
2021-05-18 18:16:23 +0100

    settings change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO flights (flight_id, source, destination, date_time, flight_num, date, time)
     VALUES
-           (1,"Dublin", "London", "2021-05-21, 14:30", "FR587L", "2021-05-21", "14:30"),
-           (2,"Dublin", "Paris", "2021-05-21, 09:00", "FR827P", "2021-05-21", "09:00"),
-           (3,"Dublin", "Madrid", "2021-05-21, 17:30", "FR256M", "2021-05-21", "17:30"),
-           (4,"Dublin", "New York", "2021-05-21, 08:30", "FR927NY", "2021-05-21", "08:30");
+           (1,"Dublin", "London", "2021-05-21 14:30", "FR587L", "2021-05-21", "14:30"),
+           (2,"Dublin", "Paris", "2021-05-21 09:00", "FR827P", "2021-05-21", "09:00"),
+           (3,"Dublin", "Madrid", "2021-05-21 17:30", "FR256M", "2021-05-21", "17:30"),
+           (4,"Dublin", "New York", "2021-05-21 08:30", "FR927NY", "2021-05-21", "08:30");
 
 
 INSERT INTO users(id,  first_name, last_name, email, address, phone_num, exec, username)
