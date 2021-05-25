1ed74d40211dd2d520657aac64c40a74fe9e62f8
magnussanne <magnussanne@me.com>
2021-05-25 14:56:27 +0100

    updates

--- src/main/java/ie/ucd/COMP47660GP/service/impl/SecurityServiceImpl.java
+++ src/main/java/ie/ucd/COMP47660GP/service/impl/SecurityServiceImpl.java
@@ -27,8 +27,6 @@ public class SecurityServiceImpl implements SecurityService {
     @Autowired
     private HttpServletRequest request;
 
-    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
-
     @Override
     public String findLoggedInEmail() {
         Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
